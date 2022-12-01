101996d35148ce98999d736d51138766ddc4f404
Saul <andrea_salvemini@yahoo.it>
2022-12-01 19:54:09 +0100

    fix tokens

--- tokens.js
+++ tokens.js
@@ -1,19 +1,19 @@
-const tokens = {
-  router: "0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3", // PCSv2 Router Testnet
-  purchaseAmount: "0.01",
-  pair: [
-    "0xae13d989dac2f0debff460ac112a837c89baa7cd", // WBNB
-    "0x8babbb98678facc7342735486c851abd7a0d17ca", // ETH
-  ],
-  gasLimit: "2000000",
-  gasPrice: "5",
-  buyDelay: 1,
-  buyRetries: 3,
-  retryMinTimeout: 250,
-  retryMaxTimeout: 3000,
-  deadline: 30,
-};
-module.exports = tokens;
+const tokens = { 
+  router: "0x10ED43C718714eb63d5aA57B78B54704E256024E", // PCSv2Router 
+  purchaseAmount: "0.00001", 
+  pair: [ 
+  "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c", // WBNB 
+  "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82", // CAKE 
+  ], 
+  gasLimit: "2000000", 
+  gasPrice: "5", 
+  buyDelay: 0, 
+  buyRetries: 1, 
+  retryMinTimeout: 250, 
+  retryMaxTimeout: 3000, 
+  deadline: 30, 
+  }; 
+  module.exports = tokens;
 
 /*
  BSC Mainnet:
